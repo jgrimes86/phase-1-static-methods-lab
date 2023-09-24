780483047fe395391d354e4c3053260ec046ee85
Jim Grimes <jimgrimes86@gmail.com>
2023-09-24 09:04:17 -0400

    completed lab

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+  static titleize(string) {
+    let array = string.toLowerCase().split(' ');
+    let newArray = array.map(item => {
+      if (`${item}` != 'a' && `${item}` != 'an' && `${item}` != 'but' && `${item}` != 'of' && `${item}` != 'and' && `${item}` != 'for' && `${item}` != 'at' && `${item}` != 'by' && `${item}` != 'from' && `${item}` != 'the') {
+        return item.charAt(0).toUpperCase() + item.slice(1);
+      } else {return item}  
+    });
+    let firstNewString = newArray.join(' ');
+    let secondNewString = firstNewString.charAt(0).toUpperCase() + firstNewString.slice(1);
+    return secondNewString;
+  }
 }
+
+// console.log(Formatter.titleize('a test And very good'))
